fa426e8b7d0ddd2ff6e481206ed8648a340925af
Preethamnikil <160018064+Preethamnikil@users.noreply.github.com>
2025-08-02 09:19:56 +0530

    wrapper

---
+++ wrapper.java
@@ -0,0 +1,14 @@
+package myproject;
+
+public class wrapper {
+     public static void main(String args[]) {
+Integer a = 564;
+Boolean b = true;
+Double c = 9.2455;
+int d = 10;
+System.out.println(a.toString().length());
+System.out.println(c.toString().length());
+System.out.println(b.toString().length());
+	 
+}
+}
